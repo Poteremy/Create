f11f98802134f02c3e46bb1b3e0a8da72e8761d9
Poteremy <68087635+Poteremy@users.noreply.github.com>
2020-07-18 21:07:15 -0400

    Update read.js

--- read.js
+++ read.js
@@ -5,30 +5,12 @@ const finalInputs = JSON.parse(localStorage.getItem('finalInputs'));
 
 title.textContent = JSON.parse(localStorage.getItem('title'));
 
-/*
 let i = 0
- function pee () {
-     
-    let textReplace = story.replace(i, finalInputs[i])
-    console.log(textReplace)
-    finalText.innerText = textReplace
-    i++
-} 
-
-finalInputs.forEach(pee)
-
-*/
-
-let i = 0
-
-
 
 finalText.innerText = story.replace(/\d+/g, replacer)
 
-function replacer (match) {
-    
+function replacer () {
     return finalInputs[i++]
-    
 }
 
 
