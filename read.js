b542a90ff7d40583e320e4c24b8204b77825f08d
Poteremy <68087635+Poteremy@users.noreply.github.com>
2020-07-18 21:01:05 -0400

    Update read.js

--- read.js
+++ read.js
@@ -5,29 +5,31 @@ const finalInputs = JSON.parse(localStorage.getItem('finalInputs'));
 
 title.textContent = JSON.parse(localStorage.getItem('title'));
 
-
-
-
 /*
+let i = 0
+ function pee () {
+     
+    let textReplace = story.replace(i, finalInputs[i])
+    console.log(textReplace)
+    finalText.innerText = textReplace
+    i++
+} 
 
-I cannot, for the life of me, figure this out. How to get the story to appear on this screen, where 
-the text within the story is replaced by corresponding values in the finalInputs array. Also, is there any way
-I can start the array at index 1? 
-
-I've gotten it to do what I want to a point, but it only displays the final iteration, replacing the LAST number identified by i
-
-As an example, currently, I have the story as a string "This is a story about 0 who 1 and 2 and 3"
-and finalInputs array is 0:one 1:two 2:three 3:four
-after the following for loop, finalText reads "This is a story about 0 who 1 and 2 and four"
+finalInputs.forEach(pee)
 
 */
 
- for(i=0; i<finalInputs.length ; i++) {
-    let textReplace = story.replace(i, finalInputs[i])
-    //console.log(textReplace)
-    finalText.innerText = textReplace
-} 
+let i = 0
 
+
+
+finalText.innerText = story.replace(/\d+/g, replacer)
+
+function replacer (match) {
+    
+    return finalInputs[i++]
+    
+}
 
 
 
